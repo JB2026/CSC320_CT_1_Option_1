f149e0eb281a4d697ba52b483f404378a4fe111a
Joshua Borck <joshua.borck@csuglobal.edu>
2024-09-14 13:35:53 -0400

    * Adding class to output person details

---
+++ src/com/CSC320_CT_1_Option_1/PersonOutput.java
@@ -0,0 +1,22 @@
+package com.CSC320_CT_1_Option_1;
+
+// A class that outputs a person's information
+public class PersonOutput {
+
+	public static void main(String[] args) {
+		// Initialize all properties
+		String firstName = "John";
+		String lastName = "Doe";
+		String address = "1357 Miller Street";
+		String city = "Lansing";
+		String zip = "48864";
+		
+		// Output to the user all of the person's info
+		System.out.println("First Name: " + firstName);
+		System.out.println("Last Name: " + lastName);
+		System.out.println("Address: " + address);
+		System.out.println("City: " + city);
+		System.out.println("Zipcode: " + zip);
+	}
+
+}
